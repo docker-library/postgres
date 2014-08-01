4540606ea2645844623e9c429b6c7d1996327888
Tianon Gravi <admwiggin@gmail.com>
2014-08-01 15:48:37 -0600

    Add more minor tweaks to generate-stackbrew-library.sh and update.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -11,12 +11,12 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-commit="$(git log -1 --format='format:%H')"
 url='git://github.com/docker-library/postgres'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
+	commit="$(git log -1 --format='format:%H' "$version")"
 	fullVersion="$(grep -m1 'ENV PG_VERSION ' "$version/Dockerfile" | cut -d' ' -f3 | cut -d- -f1 | sed 's/~/-/g')"
 	versionAliases=( ${aliases[$version]} $version $fullVersion )
 	
