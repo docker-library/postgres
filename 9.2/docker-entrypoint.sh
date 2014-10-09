4603177ae330d135dea953b42aec28fe1eef514e
Mike Dillon <mike@embody.org>
2014-10-08 20:55:25 -0700

    Add support for /docker-entrypoint-initdb.d/

--- 9.2/docker-entrypoint.sh
+++ 9.2/docker-entrypoint.sh
@@ -10,6 +10,12 @@ if [ "$1" = 'postgres' ]; then
 		sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 		
 		{ echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
+
+		if [ -d /docker-entrypoint-initdb.d ]; then
+			for f in /docker-entrypoint-initdb.d/*.sh; do
+				[ -f "$f" ] && . "$f"
+			done
+		fi
 	fi
 	
 	exec gosu postgres "$@"
