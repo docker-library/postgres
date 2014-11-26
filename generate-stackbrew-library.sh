fc445dc0b539293df9cf54bee9824ce1e3bb5e72
Tianon Gravi <admwiggin@gmail.com>
2014-11-25 22:04:02 -0700

    Fix minor "git log" usage issue

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -16,7 +16,7 @@ url='git://github.com/docker-library/postgres'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' "$version")"
+	commit="$(git log -1 --format='format:%H' -- "$version")"
 	fullVersion="$(grep -m1 'ENV PG_VERSION ' "$version/Dockerfile" | cut -d' ' -f3 | cut -d- -f1 | sed 's/~/-/g')"
 	versionAliases=( $fullVersion $version ${aliases[$version]} )
 	
