c444bceb4ffbeda91f8e406a8dc38891188ff6b6
Mike Dillon <mike@embody.org>
2015-08-21 22:57:17 -0700

    Run /docker-entrypoint-initdb.d/*.sql scripts as $POSTGRES_USER

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -75,7 +75,7 @@ if [ "$1" = 'postgres' ]; then
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
 				*.sh)  echo "$0: running $f"; . "$f" ;;
-				*.sql) echo "$0: running $f"; psql --username postgres --dbname "$POSTGRES_DB" < "$f" && echo ;;
+				*.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
 				*)     echo "$0: ignoring $f" ;;
 			esac
 			echo
