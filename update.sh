2273ec160ecfdb4554220d3164561c32f8409626
Tianon Gravi <admwiggin@gmail.com>
2015-08-21 23:14:41 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -13,6 +13,7 @@ packagesUrl='http://apt.postgresql.org/pub/repos/apt/dists/jessie-pgdg/main/bina
 packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
+travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$version\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	(
@@ -21,6 +22,11 @@ for version in "${versions[@]}"; do
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
 		sed -i 's/%%PG_MAJOR%%/'$version'/g; s/%%PG_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
 	)
+	
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
 
 rm "$packages"
