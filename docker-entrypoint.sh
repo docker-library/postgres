8e5f143bcc358930731d31087dc0b08a40a88e71
kayrus <kay.diam@gmail.com>
2016-08-17 14:47:51 +0200

    Allow run shell script before importing dump

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -76,9 +76,15 @@ if [ "$1" = 'postgres' ]; then
 		psql+=( --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" )
 
 		echo
+		for f in /docker-entrypoint-initdb.d/*.sh; do
+			echo "$0: running $f"; . "$f" ;;
+			echo
+		done
+
+		echo
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
-				*.sh)     echo "$0: running $f"; . "$f" ;;
+				*.sh)     ;; # do nothing, since we already executed scripts above
 				*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
 				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
 				*)        echo "$0: ignoring $f" ;;
