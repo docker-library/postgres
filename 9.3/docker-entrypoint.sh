4f3238cf60b514d5c20e86096817718474f53d73
Joe Ferguson <joe@infosiftr.com>
2016-12-12 10:42:05 -0800

    Run update.sh

--- 9.3/docker-entrypoint.sh
+++ 9.3/docker-entrypoint.sh
@@ -103,7 +103,7 @@ if [ "$1" = 'postgres' ]; then
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
 				*.sh)     echo "$0: running $f"; . "$f" ;;
-				*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
+				*.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
 				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
 				*)        echo "$0: ignoring $f" ;;
 			esac
