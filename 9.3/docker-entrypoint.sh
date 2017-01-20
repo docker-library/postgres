d7accc9c8c7d740ad756b887225fe31286d98f64
Tianon Gravi <admwiggin@gmail.com>
2017-01-19 16:04:43 -0800

    Add one more minor tweak on top of #253

--- 9.3/docker-entrypoint.sh
+++ 9.3/docker-entrypoint.sh
@@ -42,11 +42,11 @@ fi
 
 if [ "$1" = 'postgres' ]; then
 	mkdir -p "$PGDATA"
+	chown -R "$(id -u)" "$PGDATA" 2>/dev/null || :
+	chmod 700 "$PGDATA" 2>/dev/null || :
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
 	if [ ! -s "$PGDATA/PG_VERSION" ]; then
-		chown -R "$(id -u)" "$PGDATA" 2>/dev/null || :
-
 		file_env 'POSTGRES_INITDB_ARGS'
 		eval "initdb --username=postgres $POSTGRES_INITDB_ARGS"
 
