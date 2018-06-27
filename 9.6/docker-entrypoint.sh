a8ab11eb6333cbb4fbde8c04bb7cc108992bd8ab
Martin Catty <mcatty@synbioz.com>
2018-06-27 15:54:59 +0200

    feat: only chown files within data folder without correct ownership
    For an image already containing data this can be a huge time saver to
    only change ownership where it's needed.

--- 9.6/docker-entrypoint.sh
+++ 9.6/docker-entrypoint.sh
@@ -31,7 +31,7 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'postgres' ] && [ "$(id -u)" = '0' ]; then
 	mkdir -p "$PGDATA"
-	chown -R postgres "$PGDATA"
+	find "$PGDATA" \! -user postgres -exec chown postgres {} \;
 	chmod 700 "$PGDATA"
 
 	mkdir -p /var/run/postgresql
@@ -50,7 +50,7 @@ fi
 
 if [ "$1" = 'postgres' ]; then
 	mkdir -p "$PGDATA"
-	chown -R "$(id -u)" "$PGDATA" 2>/dev/null || :
+	find "$PGDATA" \! -user "$(id -u)" -exec chown "$(id -u)" {} \; 2>/dev/null || :
 	chmod 700 "$PGDATA" 2>/dev/null || :
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
