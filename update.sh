f3a08fbcab13c538e21e3d1fa3325f8c40b01755
Michael Käufl <dockerfiles@c.michael-kaeufl.de>
2018-07-09 16:34:51 +0200

    Bump defaultAlpineVersion to 3.8

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ defaultDebianSuite='stretch-slim'
 declare -A debianSuite=(
 	#[9.6]='jessie'
 )
-defaultAlpineVersion='3.7'
+defaultAlpineVersion='3.8'
 declare -A alpineVersion=(
 	#[9.6]='3.5'
 )
