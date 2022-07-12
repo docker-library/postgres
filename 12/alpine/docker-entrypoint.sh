9e3b61eaba4698848429fd534c40087f897ac8b9
Artem Gusintsev <32365962+Cookieees@users.noreply.github.com>
2022-07-12 15:57:00 +0500

    Update docker-entrypoint.sh
    Only the duplication of the start/stop temp_server has been eliminated

--- 12/alpine/docker-entrypoint.sh
+++ 12/alpine/docker-entrypoint.sh
@@ -306,7 +306,7 @@ _main() {
 			# then restart script as postgres user
 			exec su-exec postgres "$BASH_SOURCE" "$@"
 		fi
-
+		docker_temp_server_start "$@"
 		# only run initialization on an empty data directory
 		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
 			docker_verify_minimum_env
@@ -320,12 +320,10 @@ _main() {
 			# PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
 			# e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
 			export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-			docker_temp_server_start "$@"
 
 			docker_setup_db
 			docker_process_init_files /docker-entrypoint-initdb.d/*
 
-			docker_temp_server_stop
 			unset PGPASSWORD
 
 			echo
@@ -336,7 +334,6 @@ _main() {
 			echo 'PostgreSQL Database directory appears to contain a database; Skipping initialization'
 			echo
 		fi
-		docker_temp_server_start "$@"
 		docker_process_init_files /always-initdb.d/*
 		docker_temp_server_stop
 	fi
